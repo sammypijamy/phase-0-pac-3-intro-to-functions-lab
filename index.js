9918a829c4f07f3712ad2b9f980de0f64ddce541
+ <root@SamWitch.localdomain>
2022-06-29 13:20:51 -0700

    Done with assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log("HELLO");
+        }
+        logShout(string).toUpperCase()
+    
+function logWhisper(string) {
+    console.log("hello")
+}
+logWhisper(string).toLowerCase();
+
+function sayHiToHeadphonedRoommate(string)  { 
+if (string === string.toLowerCase() ) {
+    return "I can't hear you!"
+}
+if (string === string.toUpperCase() )  {
+    return "YES INDEED!"
+}
+if (string === "Let's have dinner together!")  {
+    return "I would love to!"
+}
+
+}
+
+
+  
